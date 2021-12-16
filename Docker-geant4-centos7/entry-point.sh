071c83c1d5a8c3b8892aaa3a8c072313bce3e4f7
maximilianKoeper <16453175+maximilianKoeper@users.noreply.github.com>
2021-12-16 01:25:38 +0100

    FIX centos7 missing boost in ld_library_path

---
+++ Docker-geant4-centos7/entry-point.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Setting up Environment Variables"
+
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH/usr/local/include/boost/lib/:"
+source /geant4-install/bin/geant4.sh
+source /root_install/bin/thisroot.sh
+source /opt/rh/devtoolset-9/enable
+
+ldconfig
+echo "Environment is ready to use"
+exec "$@"
