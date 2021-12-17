a02a010be45dd0cd5eab20e69c22f34ca795bd35
Maximilian Koeper <maximilian.koeper@kip.uni-heidelberg.de>
2021-12-17 19:56:38 +0100

    Working centos7 Version

---
+++ Docker-mu3e-centos7/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Setting up Environment Variables"
+
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/include/boost/lib/"
+source /geant4-install/bin/geant4.sh
+source /root_install/bin/thisroot.sh
+source /opt/rh/devtoolset-9/enable
+source /mu3e/install/activate.sh
+
+ldconfig
+echo "Environment is ready to use"
